fa4f39c3fee9d732d6329cbb2f30c01444822f90
Vitaliy Syrchikov <maddoger@gmail.com>
2014-10-23 20:53:34 +0400

    Заготовка.

---
+++ ImageBehavior.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @copyright Copyright (c) 2014 Vitaliy Syrchikov
+ * @link http://syrchikov.name
+ */
+
+namespace maddoger\imagecache;
+
+use maddoger\core\file\FileBehavior;
+use Yii;
+
+/**
+ * ImageUploadBehavior
+ *
+ * @author Vitaliy Syrchikov <maddoger@gmail.com>
+ * @link http://syrchikov.name
+ * @package maddoger\imagcache
+ */
+class ImageBehavior extends FileBehavior
+{
+
+}
---
+++ ImageCache.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * @copyright Copyright (c) 2014 Vitaliy Syrchikov
+ * @link http://syrchikov.name
+ */
+
+namespace maddoger\imagecache;
+use Yii;
+use yii\base\Component;
+
+/**
+ * ImageCache
+ *
+ * @author Vitaliy Syrchikov <maddoger@gmail.com>
+ * @link http://syrchikov.name
+ * @package maddoger\imagecache
+ */
+class ImageCache extends Component
+{
+    /**
+     * Path to public static files
+     * @var string
+     */
+    public $staticPath = '@static';
+
+    /**
+     * URL to static path
+     * @var string
+     */
+    public $staticUrl = '@staticUrl';
+
+    /**
+     * Path to protected static files
+     * @var string
+     */
+    public $staticProtectedPath = '@staticProtected';
+
+    /**
+     * Path to cache folder
+     * @var string
+     */
+    public $cachePath = '@static/cache';
+
+    /**
+     * URL to cache path
+     * @var string
+     */
+    public $cacheUrl = '@staticUrl/cache';
+
+    /**
+     * @var bool saving file to cache or generate only
+     */
+    public $saveFile = true;
+
+    public $presets = [
+
+    ];
+
+    public function getRawImageByPreset($preset, $image_path)
+    {
+
+    }
+}
